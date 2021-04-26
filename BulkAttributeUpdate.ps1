a725548f0ccf1e55a0b2b85c330181ad96b28546
James-Taylor94 <82955023+James-Taylor94@users.noreply.github.com>
2021-04-26 16:31:25 +0800

    Delete BulkAttributeUpdate.ps1

--- BulkAttributeUpdate.ps1
+++
@@ -1,8 +0,0 @@
-Import-Module ActiveDirectory
-$Users = Get-Content -Path 'File path'
-
-foreach ($User in $Users) {
-    Set-ADUser -Identity $User -Office 'Office Name'
-}
-
-Write-Host 'Accounts have been updated'
