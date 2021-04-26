fb4bf1242746d168093bb60500a1b820a23d6574
James-Taylor94 <82955023+James-Taylor94@users.noreply.github.com>
2021-04-26 16:32:44 +0800

    Add files via upload

---
+++ AddToADGroup.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module ActiveDirectory
+$User = Read-Host "Please enter the username"
+$Group = Read-Host "Please enter the AD group name"
+$ErrorActionPreference = 'Stop'
+
+
+Add-ADGroupMember -Identity $Group -Members $User -Verbose
+
+Write-Host "$User has been added to $Group"
