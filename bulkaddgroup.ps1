bd62f7779ed2cd59197fb7c53b8485b16ac52ddb
James-Taylor94 <82955023+James-Taylor94@users.noreply.github.com>
2021-04-26 16:30:36 +0800

    Add files via upload

---
+++ bulkaddgroup.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module ActiveDirectory
+$Users = Get-Content -Path 'C:\temp\sAMAccountname.txt'
+$ErrorActionPreference = 'Stop'
+
+foreach ($User in $Users) {
+    Add-ADGroupMember -Identity External_Users -Members $User -Verbose
+}
+
+Write-Host "$Users have been added to External_Users"
---
+++ bulkattribute.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module ActiveDirectory
+$Users = Get-Content -Path "C:\temp\sAMAccountname.txt"
+$ErrorActionPreference = 'Stop'
+
+foreach ($User in $Users) {
+    Set-ADUser -Identity $user -Office 'Wanslea -Verbose'
+}
+
+Write-Host "Accounts successfully udpated"
