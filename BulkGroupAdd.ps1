61ba809d468611a9a62c8a9e605be84005151f54
James-Taylor94 <82955023+James-Taylor94@users.noreply.github.com>
2021-04-26 16:31:17 +0800

    Delete BulkGroupAdd.ps1

--- BulkGroupAdd.ps1
+++
@@ -1,9 +0,0 @@
-Import-Module ActiveDirectory
-$Users = Get-Content -Path 'C:\temp\'
-$ErrorActionPreference = 'Stop'
-
-foreach ($User in $Users) {
-    Add-ADGroupMember - Identity External_User -Members $Users
-}
-
-Write-host 'Accounts have been added to the group'
