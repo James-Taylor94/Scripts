ad30f4e34b201688dd3271e0b61e4289413d7801
James-Taylor94 <82955023+James-Taylor94@users.noreply.github.com>
2021-05-11 10:27:53 +0800

    Update ThrimsAccountCreation.ps1

--- ThrimsAccountCreation.ps1
+++ ThrimsAccountCreation.ps1
@@ -9,19 +9,38 @@
 #>
 Import-Module ActiveDirectory
 
-$RITM= Read-Host "Enter the RITM number of the ticket"
+Function Add-LogEntry {
+    Param([ValidateSet("Error", "Info", "Warning")][String]$LogLevel, [String]$LogEntry)
+    $TimeStamp = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+    $StreamWriter = New-Object System.IO.StreamWriter -ArgumentList ([IO.File]::Open($LogFile, "Append"))
+    $StreamWriter.WriteLine("$TimeStamp - $LogLevel - $LogEntry")
+    if ($LogLevel -eq 'Error') {
+        Write-Host "$TimeStamp - $LogLevel - $LogEntry" -ForegroundColor Red
+    } elseif ($LogLevel -eq 'Warning') {
+        Write-Host "$TimeStamp - $LogLevel - $LogEntry" -ForegroundColor Yellow 
+    } elseif ($LogLevel -eq 'Info') {
+        Write-Host "$TimeStamp - $LogLevel - $LogEntry" -ForegroundColor Green
+    }
+    $StreamWriter.Close()
+}
 
+$Today = Get-Date -Format "ddMMyyyy"
+$Logfile = "\\dhw.wa.gov.au\CorporateData\IS\TSS\Support Centre\EntOps\Scripts\Logs\ThrimsAccountCreationLog_$Today.txt"
+
+$RITM= Read-Host "Enter the RITM number of the ticket"
+ 
 $SamAccountName = Read-Host "Enter the SamAccountName"
 $USER = Get-ADUser -Identity $SamAccountName | Select-Object -ExpandProperty UserPrincipalName
     if ($USER) {
-        Write-Host "$USER already has taken $SamAccountName" -ForegroundColor Red
+        Add-LogEntry -LogLevel Warning -LogEntry "$USER already has taken $SamAccountName"
      } elseif (!$USER) {
-        Write-Host "$SamAccountName is availble" -ForegroundColor Green
+        Add-LogEntry -LogLevel Info -LogEntry "$SamAccountName is availble"
     }
 
 $GivenName = Read-Host "Enter the users first name"
 $Surname = Read-Host "Enter the users surname"
 $Org = Read-Host "Enter users agency"
+$ExternalEmail = Read-Host "Enter the external email address"
 
 ### Static Variables ###
 $UserPrincipalName = "$SamAccountName@communities.wa.gov.au"
@@ -31,18 +50,18 @@ $Name = $SamAccountName
 $Admin = $env:UserName
 $Today = Get-Date -Format "dd/MM/yyyy"
 $Notes = "Created $Today $RITM $Admin"
-$Group = "External Domain Users","APP_THRIMS_USERS"
+$Group = "External Domain Users","APP_THRIMS_USERS","App-Microsoft-Office365-Communities.SecurityBaseline"
 $Password = ConvertTo-SecureString "FastAaron12" -AsPlainText -Force
 
 $ExternalPath = "OU=External_Domain_Users,OU=WXP_DMZ,OU=Housing,DC=dhw,DC=wa,DC=gov,DC=au"
 
-
+Add-LogEntry -LogLevel Info -LogEntry "Creating account..."
 New-ADUser -GivenName $GivenName -Surname $Surname `
 -SamAccountName $SamAccountName -DisplayName $DisplayName `
 -Name $Name -UserPrincipalName $UserPrincipalName -Path $ExternalPath `
 -PasswordNeverExpires $false -AccountPassword $Password -Enabled $True `
 -ChangePasswordAtLogon $True -Server $Server -Office $Org `
--Company $Org
+-Company $Org -EmailAddress $ExternalEmail
 Add-ADPrincipalGroupMembership $SamAccountName -MemberOf  $Group -Server $Server
 
 Set-ADUser -Identity $SamAccountName -Replace @{employeeType = 'External Thrims'} -Server $Server
@@ -53,4 +72,4 @@ Remove-ADGroupMember -Identity "Domain Users" -Members $SamAccountName -Server $
 
 Set-ADUser -Identity $SamAccountName -Replace @{info="$Notes;"} -Server $Server
 
-Write-Host = Get-ADUser $SamAccountName | Select-Object DisplayName,UserPrincipalName,SamAccountName
+Add-LogEntry -LogLevel info -LogEntry "Script ran by $Admin"
